b1be7eade3fe55b1cfca5791b762950be3f17ef2
tejaniv <89618457+tejaniv@users.noreply.github.com>
2022-07-30 00:03:29 -0400

    Input test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import sys
+
 from lexer import *
 from parser import *
 
 if __name__ == "__main__":
+    # Use sys.argv[x] to take input. Could use for file input
+    # sys.argv[0] will always be "./espresso" when running Espresso
     print("Welcome to Espresso")
