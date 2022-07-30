9fdc0152e37540966873f093aa03c9706afd945a
tejaniv <89618457+tejaniv@users.noreply.github.com>
2022-07-30 14:34:02 -0400

    Added token class

--- src/lexer.py
+++ src/lexer.py
@@ -1,5 +1,20 @@
+import string
 from parser import *
 
 class Token:
+    def __init__(self, type, value: string, line: int) -> None:
+        self.type = type
+        self.value = value
+        self.line = line
+
+class Tokenizer:
     def __init__(self) -> None:
-        pass
+        self.tokens = []
+        
+    
+    def tokenize(self, file):
+        self.file = open(file, "r")
+
+        # Do the tokenizing
+
+        self.file.close()
