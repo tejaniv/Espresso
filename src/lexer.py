b43624a53de02859138097ad9bcde5ea12cbad80
tejaniv <89618457+tejaniv@users.noreply.github.com>
2022-07-31 22:00:31 -0400

    Created basic lexer for basic math equations

--- src/lexer.py
+++ src/lexer.py
@@ -1,24 +1,131 @@
-import string
+from string import digits
 from parser import *
+from enum import Enum
+
+
+##############################
+# TOKEN
+##############################
+
+class Token_Types(Enum):
+    TT_IDENTIFIER       = 1
+    TT_KEYWORD          = 2
+    TT_SEPARATOR        = 3
+    TT_OPERATOR         = 4
+    TT_LITERAL          = 5
+    TT_COMMENT          = 6
 
 class Token:
-    def __init__(self, type, value: string, line: int) -> None:
+    def __init__(self, type, kind = None, value = None) -> None:
         self.type = type
         self.value = value
-        self.line = line
+        self.kind = kind
+        self.line = 1
 
     def __repr__(self) -> str:
-        return f"({self.type}: {self.value})"
+        if self.kind and self.value:
+            return f"({self.type} - {self.kind}: '{self.value}') "
+        elif self.value and not self.kind: 
+            return f"({self.type}: '{self.value}')"
+        elif self.kind and not self.value:
+            return f"({self.type} - {self.kind})"
+        return f"{self.type}"
 
+
+##############################
+# LEXER
+##############################
+        
 class Lexer:
-    def __init__(self, data) -> None:
-        self.data = data
+    def __init__(self) -> None:
         self.tokens = []
+        
+        self.keywords = {
+            'print': "PRINT"
+        }
+        self.separators = {
+            '(': "LPAREN",
+            ')': "RPAREN",
+        }
+        self.operators = {
+            '+': "PLUS",
+            '-': "MINUS",
+            '*': "MUL",
+            '/': "DIV",
+        }
+        self.literals = {
+            'int': "INT",
+            'float': "FLOAT",
+            'bool': "BOOLEAN",
+            'str': "STRING",
+            'list': "LIST",
+            'char': "CHARACTER"
+        }
 
-    
-    def tokenize(self, file):
-        self.file = open(file, "r")
+        self.booleans = ["true", "false"]
 
-        # Do the tokenizing
+    def tokenize_terminal(self, line):
+        first_index = 0
+        temp_type = ''
+        self.pos = 0
+        self.cur_char = None
 
-        self.file.close()
+        while self.pos <= len(line)-1:
+            print()
+            print("pos: ", self.pos)
+            print("new char: ", line[self.pos])
+            if line[self.pos] == ' ' or line[self.pos] == '\t':
+                print("space found")
+                self.cur_char = None
+                temp_type = ''
+                self.pos += 1
+            else:
+                self.cur_char = line[self.pos]
+                temp_type, temp_kind = self.find_type(self.cur_char)
+
+                first_index = self.pos
+                
+                self.find_end_of_type(temp_type, temp_kind, line)
+
+                print("first: ", first_index, " last: ", self.pos)
+                print("sub-line", line[first_index:self.pos])
+                print("temp_type: ", temp_type)
+
+            if temp_type == Token_Types.TT_LITERAL:
+                self.tokens.append(Token(temp_type, line[first_index:self.pos]))
+            elif temp_type == Token_Types.TT_OPERATOR:
+                self.tokens.append(Token(temp_type, temp_kind, line[first_index:self.pos]))
+            
+            temp_type = ''
+            
+        print(self.tokens)
+            
+    def find_type(self, temp_str):
+        print("temp_str: ", temp_str)
+        #print("is numeric: ",temp_str.isnumeric())
+        print("operator?: ", ('+' in self.operators))
+
+        if temp_str in self.keywords:
+            return Token_Types.TT_KEYWORD, self.keywords[temp_str]
+        elif temp_str in self.separators:
+            return Token_Types.TT_SEPARATOR, self.separators[temp_str]
+        elif temp_str in self.operators:
+            return Token_Types.TT_OPERATOR, self.operators[temp_str]
+        elif (temp_str.isnumeric()) or (temp_str in self.booleans):
+            return Token_Types.TT_LITERAL, "INT" # Change later
+        else:
+            return Token_Types.TT_IDENTIFIER, None
+         
+
+    def find_end_of_type(self, type, kind, line):
+        print("finding end for ", type, " type")
+        temp_str = ''
+        cur_type = type
+        cur_kind = kind
+
+        while (type == cur_type) and (kind == cur_kind) and (self.pos <= len(line)-1):
+            temp_str += line[self.pos]
+            cur_type, cur_kind = self.find_type(temp_str)
+            self.pos += 1
+
+        print("new pos: ", self.pos)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from parser import *
 
 def process_terminal(line):
     print(line)
-    lexer.tokenize(line)
+    lexer.tokenize_terminal(line)
     # Create tokens
     # Run them through parser
     # Evaluate
@@ -18,22 +18,22 @@ def process_file(file):
 
 def run_terminal():
     print("Welcome to the Espresso Terminal! \nEnter .q or .quit to exit the terminal")
-    user_input = ""
+    user_input = "123 - 5"
 
-    while (not user_input.strip() == ".q") and (not user_input.strip() == ".quit"):
-        user_input = input("Esp >> ")
-        process_terminal(user_input)
+    
+    #while (not user_input.strip() == ".q") and (not user_input.strip() == ".quit"):
+    #    user_input = input("Esp >> ")
+    process_terminal(user_input)
     
 
 if __name__ == "__main__":
     # Use sys.argv[x] to take input. Could use for file input
     # sys.argv[0] will always be "./espresso" when running Espresso
-    
+    print("Welcome to Espresso")
+
     lexer = Lexer()
 
-    #if len(sys.argv) == 1:
-    #    run_terminal()
+    if len(sys.argv) == 1:
+        run_terminal()
     #elif len(sys.argv) == 2:
     #    process_file(sys.argv[1])
-
-    print("Welcome to Espresso")
